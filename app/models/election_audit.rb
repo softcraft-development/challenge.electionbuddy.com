11694e44921d89c95311e417417a98c7169afc52
Craig Walker <github@softcraft.ca>
2021-01-24 15:06:52 -0700

    record audit of election changes

--- app/models/election_audit.rb
+++ app/models/election_audit.rb
@@ -1,3 +1,16 @@
+# In real life, I probably wouldn't write this data to the main database.
+# Audit trails should be write-once and *never* updated after the fact.
+# Thus, we don't need the ACID-level updateability that an RDBMS provides.
+#
+# Combined with the fact that these audits will effectively double the number
+# of writes, it's likely that ElectionAudit should be persisted to something
+# else (perhaps something like AWS kinesis, S3, or even flat log files).
+#
+# Changing the audit persistence mechansism may also give us the opportunity
+# to do some sort of cryptographic validation (ie: digital signatures.)
+# If this was 2015, I might even propose blockchain (and then call my broker).
+#
+# However, for this challenge, saving ElectionAudit to its own table is good enough.
 class ElectionAudit < ApplicationRecord
   belongs_to :election
   belongs_to :user
